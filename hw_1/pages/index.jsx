e33c0a0848e32998c11f65c74a87e0fa446ba741
fjellrevv <zzhilinko@yandex.ru>
2022-11-08 17:34:52 +0300

    add dz_1

---
+++ hw_1/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Text } from '../components/Text/Text';
+
+export default function IndexPage() {
+
+  return (
+    <main>
+      <Text>Главная страница</Text>
+    </main>
+  );
+}
